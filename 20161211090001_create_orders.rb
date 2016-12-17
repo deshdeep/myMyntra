ae12014243879f5eaf78fba29829ffef5ee0400e
Deshdeep <deshdeep@users.noreply.github.com>
2016-12-17 16:55:37 +0530

    Delete 20161211090001_create_orders.rb

--- 20161211090001_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration
-  def change
-    create_table :orders do |t|
-	  t.integer  :order_quantity
-	  t.float    :gross_total
-      t.timestamps null: false
-    end
-  end
-end
