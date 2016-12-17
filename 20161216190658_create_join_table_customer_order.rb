385d4a9f7beb7f408e49e0e855b78d93f84256de
Deshdeep <deshdeep@users.noreply.github.com>
2016-12-17 16:54:40 +0530

    Delete 20161216190658_create_join_table_customer_order.rb

--- 20161216190658_create_join_table_customer_order.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCustomerOrder < ActiveRecord::Migration
-  def change
-    create_join_table :customers, :orders do |t|
-      t.belongs_to :customer, index: true
-      t.belongs_to :order, index: true
-    end
-  end
-end
