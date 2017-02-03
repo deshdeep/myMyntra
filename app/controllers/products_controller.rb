7fda4148014a8bd5f5d7c44a53419645d806b768
Deshdeep <deshdeep@users.noreply.github.com>
2017-02-03 18:50:03 +0530

    Added basic functions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,42 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
+	
+	def show
+		@product = Product.find(params[:id])
+	end
+	
+	def new
+		@product = Product.new
+	end
+	
+	def edit
+		@product = Product.find(params[:id])
+	end
+	
+	def create
+		@product = Product.new(product_params)
+		
+		if @product.save
+			redirect_to @product
+		else
+			render 'new'
+		end
+	end
+	
+	def update 
+		@product = Product.find(:id)
+		
+		if @product.update(product_params)
+			redirect_to @product
+		else
+			render 'edit'
+		end
+	end
+	
+private
+		def product_params
+			params.require(:product).permit(:name,:brand,:price,:size,:category) 
+		end
 end
