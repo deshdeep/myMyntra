0f6198b9dda559e1511c49cbba680f0d1b79c1b7
Deshdeep <deshdeep@users.noreply.github.com>
2017-02-06 13:18:08 +0530

    added destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,13 +26,20 @@ class ProductsController < ApplicationController
 	end
 	
 	def update 
-		@product = Product.find(:id)
+		@product = Product.find(params[:id])
 		
 		if @product.update(product_params)
 			redirect_to @product
 		else
 			render 'edit'
 		end
+	end
+	
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+	
+		redirect_to products_path
 	end
 	
 private
