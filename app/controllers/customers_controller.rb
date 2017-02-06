f80d5f119b392b79cdd63837a13c66997173364a
Deshdeep <deshdeep@users.noreply.github.com>
2017-02-06 13:19:25 +0530

    added crud definitions

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,48 @@
 class CustomersController < ApplicationController
+
+	def index
+		@customers = Customer.all
+	end
+	
+	def show
+		@customer = Customer.find(params[:id])
+	end
+	
+	def new
+		@customer = Customer.new
+	end
+	
+	def edit
+		@customer = Customer.find(params[:id])
+	end
+	
+	def create
+		@customer = Customer.new(customer_params)
+		if @customer.save
+			redirect_to @customer
+		else
+			render 'new'
+		end	
+	end
+	
+	def update
+		@customer = Customer.find(params[:id])
+		if @customer.update(customer_params)
+			redirect_to @customer
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@customer = Customer.find(params[:id])
+		@customer.destroy
+		
+		redirect_to customers_path
+	end
+	
+private 
+	def customer_params
+		params.require(:customer).permit(:first_name,:last_name,:email,:mobile_number)
+	end
 end
