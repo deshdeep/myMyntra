dfbba2d31e0309f2cf4dbb421f73876faab6665f
deshdeep <deshdeeptherock@gmail.com>
2017-02-15 19:35:57 +0530

    added cart model and migration files

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ActiveRecord::Base
+	has_and_belongs_to_many :products
+	has_and_belongs_to_many :orders
+	has_one :cart
+	has_many :delivery_addresses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216190658) do
+ActiveRecord::Schema.define(version: 20170215113706) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
